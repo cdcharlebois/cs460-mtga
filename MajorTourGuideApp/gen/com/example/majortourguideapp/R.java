ca69082f7bd5188651fb72878d168232fdbe0c39
Conner Charlebois <cdcharlebois@gmail.com>
2013-03-06 20:35:24 -0500

    created Fork.java

--- MajorTourGuideApp/gen/com/example/majortourguideapp/R.java
+++ MajorTourGuideApp/gen/com/example/majortourguideapp/R.java
@@ -15,7 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btnCore=0x7f070004;
+        public static final int btnFaculty=0x7f070003;
+        public static final int btnFind=0x7f070002;
+        public static final int lblMajor=0x7f070001;
+        public static final int llButtonWrapper=0x7f070000;
+        public static final int menu_settings=0x7f070005;
     }
     public static final class layout {
         public static final int activity_courses=0x7f030000;
@@ -32,6 +37,7 @@ public final class R {
         public static final int activity_welcome_menu=0x7f060004;
     }
     public static final class string {
+        public static final int Major=0x7f040008;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
--- MajorTourGuideApp/src/com/example/majortourguideapp/Fork.java
+++ MajorTourGuideApp/src/com/example/majortourguideapp/Fork.java
@@ -1,15 +1,33 @@
+/*
+ * This Activity is launched from the Main App Screen, and presents the user with 3
+ * buttons, each of which launches a new activity.
+ */
 package com.example.majortourguideapp;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Fork extends Activity {
-
+public class Fork extends Activity implements OnClickListener {
+	
+	//Declare Instance Variables
+	private Button btnFind, btnFaculty, btnCore;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fork);
+		btnFind = (Button) findViewById(R.id.btnFind);
+		btnFaculty = (Button) findViewById(R.id.btnFaculty);
+		btnCore = (Button) findViewById(R.id.btnCore);
+		
+		btnFind.setOnClickListener(this);
+		btnFaculty.setOnClickListener(this);
+		btnCore.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +35,27 @@ public class Fork extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_fork, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		String name = "";
+		switch (v.getId()){
+		case R.id.btnFind:
+			//Do some stuff
+			name="Find";
+			break;
+		case R.id.btnFaculty:
+			//Do some different stuff
+			name="Faculty";
+			break;
+		case R.id.btnCore:
+			//Do some crazy stuff
+			name="Core";
+			break;
+		}
+		Toast.makeText(this, "You started this activity: "+name, Toast.LENGTH_LONG).show();
+		
 	}
 
 }
