d4d6df4d9e03d5a503e01e6490ef5a393f8c7cd2
Robert Dextradeur <dextrad_robe@bentley.edu>
2013-03-22 17:34:16 -0400

    added welcome menu functionality and layout

--- MajorTourGuideApp/src/com/example/majortourguideapp/WelcomeMenu.java
+++ MajorTourGuideApp/src/com/example/majortourguideapp/WelcomeMenu.java
@@ -7,30 +7,48 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class WelcomeMenu extends Activity {
+public class WelcomeMenu extends Activity implements OnClickListener {
+	
+	//Declare Instance Variables
+	private Button btnCIS, btnFI, btnAC, btnMK;
+	private int major;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome_menu);
+		
+		//inflate layout
+		btnCIS = (Button) findViewById(R.id.btnCIS);
+		btnFI = (Button) findViewById(R.id.btnFI);
+		btnAC = (Button) findViewById(R.id.btnAC);
+		btnMK = (Button) findViewById(R.id.btnMK);
+		
+		//set listeners
+		btnCIS.setOnClickListener(this);
+		btnFI.setOnClickListener(this);
+		btnAC.setOnClickListener(this);
+		btnMK.setOnClickListener(this);
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_welcome_menu, menu);
-		next();
+		//next();
 		return true;
 	}
-	
-	
 	/**
 	 * This method is called when the user selects a major
 	 * @param int major: the major_id of the selected major 
 	 * 		=> CIS=1, ACCOUNTING=2, FINANCE=3, MARKETING=4
 	 */
-	public void next(/*int major*/){
+	public void next(int major){
 		//explicit intent for an Activity in another application
         //format is ComponentName(package, class)
         //be sure other app has been run at least once so that its package
@@ -41,10 +59,31 @@ public class WelcomeMenu extends Activity {
 
        	// ========== vvv ==========
        	// this line sends the major selected to the next activity
-       	i.putExtra("major", 1/* major */);	//change to whatever major they select.
+       	i.putExtra("major", major);	//change to whatever major they select.
         // ========== ^^^ ==========
        	
        	startActivity(i);
 	}
+	
+	
+	public void onClick(View v) {
+		String name = "";
+		Intent i = new Intent();
+		switch (v.getId()){
+		case R.id.btnCIS:
+			next(major = 1);
+			break;
+		case R.id.btnFI:
+			next(major = 3);
+			break;
+		case R.id.btnAC:
+			next(major = 2);
+			break;
+		case R.id.btnMK:
+			next(major = 4);
+			break;
+			}
+	}
+
 
 }
