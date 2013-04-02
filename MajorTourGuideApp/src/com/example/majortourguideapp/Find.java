14aa5d377a008867bd5610f49426ae131b684a56
Conner Charlebois <cdcharlebois@gmail.com>
2013-04-02 18:25:52 -0400

    GOOGLE MAPS WORKS

--- MajorTourGuideApp/src/com/example/majortourguideapp/Find.java
+++ MajorTourGuideApp/src/com/example/majortourguideapp/Find.java
@@ -1,20 +1,35 @@
 package com.example.majortourguideapp;
 
+import java.util.ArrayList;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.CameraUpdateFactory;
 
 public class Find extends Activity {
 	private Spinner spinner;
 	int major;
 	private ImageView img;
+	private ArrayList<String> destinations = new ArrayList<String>();
+	private ArrayAdapter<String> aa;
+	private GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +45,33 @@ public class Find extends Activity {
 
 		//inflate layout
 		spinner = (Spinner) findViewById(R.id.spinner1);
-		img = (ImageView)findViewById(R.id.image);
-        
+//		img = (ImageView)findViewById(R.id.image);
+		map = ((MapFragment)getFragmentManager(). findFragmentById(R.id.map)).getMap();
+		
+
 		
 		//initialize
 		major = getIntent().getExtras().getInt("major");
-		img.setImageResource(R.drawable.nav);
+		map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 		
 		//populate the list of destinations from the major
-		ArrayAdapter<CharSequence> aa = null;
-		switch(major){
-		case 1:
-			aa = ArrayAdapter.createFromResource(this, R.array.CIS_destinations, android.R.layout.simple_spinner_item);
-			break;
-		}
+		DB_Helper db = new DB_Helper(this);
+		Cursor c = db.selectFromXwhereY(DB_Contract.Destination.TABLE_NAME, DB_Contract.Destination.COLUMN_MAJOR+" = "+major);
+		c.moveToFirst();
+		do{
+			String destName = c.getString(c.getColumnIndexOrThrow(DB_Contract.Destination.COLUMN_NAME));
+			destinations.add(destName);
+			c.moveToNext();
+		}while(!c.isAfterLast());
+		db.close();
+		
+		//assign the arrayList to the adapter.
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(
+				this,
+				android.R.layout.simple_list_item_1,
+				destinations);
+		
+		
 		aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(aa);
 	}
