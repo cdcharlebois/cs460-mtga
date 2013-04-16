cb18448eb775ec17f540cd898cd8315e2601feec
Conner Charlebois <cdcharlebois@gmail.com>
2013-04-16 15:55:59 -0400

    FIXED - USE THIS

--- MajorTourGuideApp/gen/com/example/majortourguideapp/R.java
+++ MajorTourGuideApp/gen/com/example/majortourguideapp/R.java
@@ -276,40 +276,40 @@ containing a value of this type.
     }
     public static final class id {
         public static final int action_settings=0x7f040023;
-        public static final int btnAC=0x7f040015;
-        public static final int btnCIS=0x7f040014;
+        public static final int btnAC=0x7f040016;
+        public static final int btnCIS=0x7f040015;
         public static final int btnContinue=0x7f040010;
         public static final int btnCore=0x7f04000c;
-        public static final int btnFI=0x7f040017;
+        public static final int btnFI=0x7f040018;
         public static final int btnFaculty=0x7f04000b;
         public static final int btnFind=0x7f04000a;
         public static final int btnGPS=0x7f04000f;
         public static final int btnInfo=0x7f04000e;
-        public static final int btnMK=0x7f040018;
+        public static final int btnMK=0x7f040019;
         public static final int call=0x7f040021;
-        public static final int email=0x7f04001d;
+        public static final int email=0x7f04001f;
         public static final int fullscreen_content=0x7f040011;
         public static final int hybrid=0x7f040004;
-        public static final int lblChoice=0x7f040012;
+        public static final int lblChoice=0x7f040013;
         public static final int lblMajor=0x7f040009;
-        public static final int list_image=0x7f04001a;
+        public static final int list_image=0x7f04001b;
         public static final int llButtonWrapper=0x7f040008;
-        public static final int llButtonWrapper1=0x7f040013;
-        public static final int llButtonWrapper2=0x7f040016;
+        public static final int llButtonWrapper1=0x7f040014;
+        public static final int llButtonWrapper2=0x7f040017;
         public static final int map=0x7f040007;
         public static final int map_blurb=0x7f040006;
         public static final int menu_settings=0x7f040020;
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
-        public static final int office=0x7f04001c;
+        public static final int office=0x7f04001d;
         public static final int phone=0x7f04001e;
-        public static final int profile=0x7f04001f;
+        public static final int picLogo=0x7f040012;
         public static final int satellite=0x7f040002;
         public static final int spinner1=0x7f040005;
         public static final int splashBtnWrapper=0x7f04000d;
         public static final int terrain=0x7f040003;
-        public static final int thumbnail=0x7f040019;
-        public static final int title=0x7f04001b;
+        public static final int thumbnail=0x7f04001a;
+        public static final int title=0x7f04001c;
         public static final int web=0x7f040022;
     }
     public static final class layout {
