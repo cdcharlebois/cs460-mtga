c4b0399ad680cbde88310c01f38549b24e2d0930
Conner Charlebois <cdcharlebois@gmail.com>
2013-04-17 00:25:54 -0400

    added new button resources

--- MajorTourGuideApp/src/com/example/majortourguideapp/SplashScreen.java
+++ MajorTourGuideApp/src/com/example/majortourguideapp/SplashScreen.java
@@ -1,17 +1,30 @@
+/**	====== ================== ======
+ * 	------	SPLASHSCREEN.JAVA ------
+ * 	====== ================== ======
+ * 
+ * 	=== MODIFICATIONS ===
+ * 	04/17/2013 -> added logo to layout, added new buttons, attempted to animate
+ */
+
 package com.example.majortourguideapp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Button;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class SplashScreen extends Activity implements OnClickListener {
-	private Button btnGPS, btnContinue, btnInfo;
+	private Button btnContinue, btnInfo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,29 +33,34 @@ public class SplashScreen extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_splash_screen);
 		
 		//inflate
-		btnGPS = (Button)findViewById(R.id.btnGPS);
+		
 		btnContinue = (Button)findViewById(R.id.btnContinue);
 		btnInfo = (Button)findViewById(R.id.btnInfo);
 		
 		//set listeners
-		btnGPS.setOnClickListener(this);
 		btnContinue.setOnClickListener(this);
 		btnInfo.setOnClickListener(this);
 	}
 
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		/* --- cdc attempt at faking animation --- */
+		v.setBackground(getResources().getDrawable(R.drawable.button_pressed));
+		/*for(int i=0; i<100000; i++){
+			float nothing = 3.145f / (float)Math.random()+1;	//100k flops -- just to kill time
+			Log.i("cdc", "hello "+i);
+		}*/
+		v.setBackground(getResources().getDrawable(R.drawable.button));
+		/* --- /attempt -- */
 		switch(v.getId()){
 		case R.id.btnContinue:
+			
+			
 			//if GPS is not enabled, prompt to enable
 			
 			// --- /prompt ---
 			goToNext();
-			break;
-		case R.id.btnGPS:
-			//prompt to turn on GPS
-			turnGPSOn();
 			break;
 		case R.id.btnInfo:
 			//do some stuff
@@ -73,4 +91,6 @@ public class SplashScreen extends Activity implements OnClickListener {
 	        sendBroadcast(poke);
 	    }
 	}
+	
+	
 }
